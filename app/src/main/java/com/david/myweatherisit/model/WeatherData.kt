ab2267997e9c6283db72ff70a34926893a1b84df
David Rodriguez <rgomar90@gmail.com>
2024-07-18 15:39:53 -0700

    Add class for fill response, and the weather class for send http request

--- app/src/main/java/com/david/myweatherisit/model/WeatherData.kt
+++ app/src/main/java/com/david/myweatherisit/model/WeatherData.kt
@@ -1,13 +1,7 @@
 package com.david.myweatherisit.model
 
-import android.content.Context
-import android.view.View
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import com.david.myweatherisit.R
 import com.david.myweatherisit.model.apiclient.KtorClient
 import com.david.myweatherisit.model.dto.CityResponse
-import com.david.myweatherisit.view.details.DetailsFragment
 
 class WeatherData {
     val api: KtorClient = KtorClient()
