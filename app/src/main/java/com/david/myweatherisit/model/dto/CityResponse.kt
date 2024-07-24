09c00fa9fdc1c62c6460b676c5e32ec21d1a4140
David Rodriguez <rgomar90@gmail.com>
2024-07-24 11:25:52 -0700

    Add location permission, new version of CityResponse class, add xml for preference

--- app/src/main/java/com/david/myweatherisit/model/dto/CityResponse.kt
+++ app/src/main/java/com/david/myweatherisit/model/dto/CityResponse.kt
@@ -4,64 +4,36 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CityResponse (
-    val data: Array<Data>
-)
-@Serializable
-data class Data(
-    @SerialName("wind_cdir")
-    val windCdir: String? = null,
-    val rh: Double? = null,
-    val pod: String? = null,
-    val lon: Double? = null,
-    val pres: Double? = null,
-    val timezone: String? = null,
-    @SerialName("ob_time")
-    val obTime: String? = null,
-    @SerialName("country_code")
-    val countryCode: String? = null,
-    val clouds: Double? = null,
-    val vis: Double? = null,
-    @SerialName("wind_spd")
-    val windSpd: Double? = null,
-    val gust: Double? = null,
-    @SerialName("wind_cdir_full")
-    val windCdirFull: String? = null,
-    @SerialName("app_temp")
-    val appTemp: Double? = null,
-    @SerialName("state_code")
-    val stateCode: String? = null,
-    val ts: Double? = null,
-    @SerialName("h_angle")
-    val hAngle: Double? = null,
-    val dewpt: Double? = null,
-    val weather: Weather,
-    val uv: Double? = null,
-    val aqi: Double? = null,
-    val station: String? = null ,
-    val sources: List<String>? = null,
-    @SerialName("wind_dir")
-    val windDir: Double? = null,
-    @SerialName("elev_angle")
-    val elevAngle: Double? = null,
-    val datetime: String? = null,
-    val precip: Double? = null,
-    val ghi: Double? = null,
-    val dni: Double? = null,
-    @SerialName("solar_rad")
-    val solarRad: Double? = null,
-    @SerialName("city_name")
-    val cityName: String,
-    val sunrise: String? = null,
-    val sunset: String? = null,
-    val temp: Double? = null,
-    val lat: Double? = null,
-    val slp: Double? = null,
+data class CityResponse(
+    val location: Location,
+    val current: Current,
 )
 
 @Serializable
-data class Weather(
-    val icon: String? = null,
-    val code: Double? = null,
-    val description: String? = null,
+data class Location(
+    val name: String,
+    val region: String,
+    val country: String,
+    val lat: Double,
+    val lon: Double,
+    @SerialName("tz_id")
+    val tzId: String,
+    @SerialName("localtime_epoch")
+    val localtimeEpoch: Long,
+    val localtime: String,
+)
+
+@Serializable
+data class Current(
+    @SerialName("temp_c")
+    val tempC: Double,
+    @SerialName("temp_f")
+    val tempF: Double,
+    val condition: Condition,
+    val humidity: Long,
+    val cloud: Long,
+)
+@Serializable
+data class Condition(
+    val icon: String,
 )
